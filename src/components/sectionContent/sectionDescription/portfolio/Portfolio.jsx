55db5c59eaecefb165a12a96f32686c627ddf9ef
Bartosz Zawadka <bart.zaw00@gmail.com>
2023-03-06 22:38:13 +0100

    Portfolio Card started

--- src/components/sectionContent/sectionDescription/portfolio/Portfolio.jsx
+++ src/components/sectionContent/sectionDescription/portfolio/Portfolio.jsx
@@ -1,11 +1,78 @@
-import React from 'react'
+import React, { useState } from "react";
 
-import "./portfolio.css"
+import "./portfolio.css";
 
 const Portfolio = () => {
-  return (
-    <div>Portfolio</div>
-  )
-}
+  const [tab, setTab] = useState(0);
 
-export default Portfolio
+  return (
+    <div className="section__right_content_description_portfolio">
+      <div className="section__right_content_description_portfolio_header">
+        <a
+          style={{ color: tab === 0 ? "rgb(250, 191, 48)" : "#000" }}
+          className="section__right_content_description_portfolio_header_tag"
+          onClick={() => setTab(0)}
+        >
+          REACT
+        </a>
+        <a
+          style={{ color: tab === 1 ? "rgb(250, 191, 48)" : "#000" }}
+          className="section__right_content_description_portfolio_header_tag"
+          onClick={() => setTab(1)}
+        >
+          JAVASCRIPT
+        </a>
+        <a
+          style={{ color: tab === 2 ? "rgb(250, 191, 48)" : "#000" }}
+          className="section__right_content_description_portfolio_header_tag"
+          onClick={() => setTab(2)}
+        >
+          HTML + CSS
+        </a>
+      </div>
+      <div className="section__right_content_description_portfolio_details">
+        <div className="section__right_content_description_portfolio_details_box">
+          <img
+            className="section__right_content_description_portfolio_details_box_photo"
+            src="/src/assets/html_css_1.png"
+            alt="project-image"
+          />
+          <div class="section__right_content_description_portfolio_details_box_overlay"></div>
+          <div className="section__right_content_description_portfolio_details_box_description">
+            <h3>Hair Styling</h3>
+            <div className="section__right_content_description_portfolio_details_box_description_divider"></div>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</p>
+          </div>
+        </div>
+        <div className="section__right_content_description_portfolio_details_box">
+          <img
+            className="section__right_content_description_portfolio_details_box_photo"
+            src="/src/assets/html_css_1.png"
+            alt="project-image"
+          />
+          <div class="section__right_content_description_portfolio_details_box_overlay"></div>
+          <div className="section__right_content_description_portfolio_details_box_description">
+            <h3>Hair Styling</h3>
+            <div className="section__right_content_description_portfolio_details_box_description_divider"></div>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</p>
+          </div>
+        </div>
+        <div className="section__right_content_description_portfolio_details_box">
+          <img
+            className="section__right_content_description_portfolio_details_box_photo"
+            src="/src/assets/html_css_1.png"
+            alt="project-image"
+          />
+          <div class="section__right_content_description_portfolio_details_box_overlay"></div>
+          <div className="section__right_content_description_portfolio_details_box_description">
+            <h3>Hair Styling</h3>
+            <div className="section__right_content_description_portfolio_details_box_description_divider"></div>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Portfolio;
--- src/components/sectionContent/sectionDescription/resume/Resume.jsx
+++ src/components/sectionContent/sectionDescription/resume/Resume.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { GiConfirmed } from "react-icons/gi";
 
 import "./resume.css";
 
@@ -13,7 +14,13 @@ const Resume = () => {
           <span className="section__right_content_description_resume_details_detail_time">
             October 2019 - now
           </span>
-          <span>Applied Computer Science | <br/>Lodz University of Technology</span>
+          <span className="section__right_content_description_resume_details_detail_title">
+            Applied Computer Science | <br />
+            <span className="section__right_content_description_resume_details_detail_title section__right_content_description_resume_details_detail_title--weightLow">
+              {" "}
+              Lodz University of Technology
+            </span>
+          </span>
         </div>
       </div>
       <div className="section__right_content_description_resume_divider"></div>
@@ -25,18 +32,34 @@ const Resume = () => {
           <span className="section__right_content_description_resume_details_detail_time">
             August 2021 - October 2022
           </span>
-          <span>Sales and Customer Service Consultant | <br/>Tutore</span>
-          <ul>
-            <li>High sales results</li>
-            <li>Developing soft skills</li>
+          <span className="section__right_content_description_resume_details_detail_title">
+            Sales and Customer Service Consultant | <br />
+            <span className="section__right_content_description_resume_details_detail_title section__right_content_description_resume_details_detail_title--weightLow">
+              {" "}
+              Tutore
+            </span>
+          </span>
+          <ul className="section__right_content_description_resume_details_detail_list">
+            <li className="section__right_content_description_resume_details_detail_list_element">
+              High sales results
+            </li>
+            <li className="section__right_content_description_resume_details_detail_list_element">
+              Developing soft skills
+            </li>
           </ul>
         </div>
         <div className="section__right_content_description_resume_details_detail">
           <span className="section__right_content_description_resume_details_detail_time">
             January 2019 - August 2021
           </span>
-          <span>Wordpress Developer | freelancing</span>
-          <ul>
+          <span className="section__right_content_description_resume_details_detail_title">
+            Wordpress Developer |
+            <span className="section__right_content_description_resume_details_detail_title section__right_content_description_resume_details_detail_title--weightLow">
+              {" "}
+              freelancing
+            </span>
+          </span>
+          <ul className="section__right_content_description_resume_details_detail_list">
             <li>Web design in WordPress and WooCommerce</li>
           </ul>
         </div>
@@ -50,8 +73,14 @@ const Resume = () => {
           <span className="section__right_content_description_resume_details_detail_time">
             September 2021 - June 2022
           </span>
-          <span>Academy Scout | Widzew Lodz</span>
-          <ul>
+          <span className="section__right_content_description_resume_details_detail_title">
+            Academy Scout |
+            <span className="section__right_content_description_resume_details_detail_title section__right_content_description_resume_details_detail_title--weightLow">
+              {" "}
+              Widzew Lodz
+            </span>
+          </span>
+          <ul className="section__right_content_description_resume_details_detail_list">
             <li>Working in a group</li>
             <li>Searching for football talents in my area</li>
             <li>Preparation of match reports</li>
