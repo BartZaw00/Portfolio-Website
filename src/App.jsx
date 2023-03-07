509aa2c980fb5b295f4a891d6c9c7ec2208946b6
Bartosz Zawadka <bart.zaw00@gmail.com>
2023-03-07 17:25:27 +0100

    Pressing arrow keys and scrolling mouse events added

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { createContext, useEffect, useState } from "react";
+import { createContext, useCallback, useEffect, useState } from "react";
 import { NavigationBar } from "./components";
 
 import { MainCard, SectionCard } from "./containers";
@@ -10,9 +10,36 @@ export const PageContext = createContext();
 function App() {
   const [page, setPage] = useState(0);
 
+  const handleKeyPress = useCallback(
+    (event) => {
+      if (event.keyCode === 40 || event.keyCode === 39) {
+        if (page === 4) return;
+        setPage(page + 1); // down or left arrow key
+      }
+      if (event.keyCode === 38 || event.keyCode === 37) {
+        if (page === 0) return;
+        setPage(page - 1); // up or right arrow key
+      }
+    },
+    [page]
+  );
+
+  const handleWheel = useCallback(
+    (event) => {
+      if (event.deltaY > 0 && page < 4) setPage(page + 1); // mouse scroll down
+      if (event.deltaY < 0 && page > 0) setPage(page - 1); // mouse scroll up
+    },
+    [page]
+  );
+
   useEffect(() => {
-    console.log(page);
-  }, [page]);
+    window.addEventListener("keydown", handleKeyPress);
+    window.addEventListener("wheel", handleWheel);
+    return () => {
+      window.removeEventListener("keydown", handleKeyPress);
+      window.removeEventListener("wheel", handleWheel);
+    };
+  }, [handleKeyPress, handleWheel]);
 
   return (
     <PageContext.Provider value={[page, setPage]}>
